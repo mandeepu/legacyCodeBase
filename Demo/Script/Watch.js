2b33005e4865ebb6a82debe5841ff5bb88656e14
mandeep uppal <mandeep.uppal@mergermarket.com>
2014-05-12 15:49:14 +0100

    inital checkin to my repository

---
+++ Demo/Script/Watch.js
@@ -0,0 +1,24 @@
+﻿var myapp = angular.module('watchApp', []);
+
+myapp.controller('TextWatchCtrl', [
+        '$scope', function($scope) {
+            //var scope = $scope;
+            $scope.watchtext = "first value";
+            $scope.counter = 0;
+            $scope.lastvalue = "";
+            $scope.currentvalue = "";
+
+            $scope.initCounter = function () {
+                $scope.counter = 0;
+            };
+
+            $scope.$watch('watchtext', function (newvalue, oldvalue) {
+                $scope.counter = $scope.counter + 1;
+                $scope.lastvalue = oldvalue;
+                $scope.currentvalue = newvalue;
+
+            });
+
+
+        }
+    ]);
---
+++ Demo/Script/directive.js
@@ -0,0 +1,65 @@
+﻿///<reference path="../Script/angular.min.js" /> 
+
+var myapp = angular.module('directiveApp', []);
+
+myapp.controller('fundooCtrl', function($scope, $window) {
+    $scope.rating = 5;
+    $scope.saveRatingToServer = function(rating) {
+        $window.alert("Rating selected - " + rating); 
+    };
+
+});
+
+myapp.directive('span', function() {
+    var directive = {};
+
+    directive.restrict = 'E';
+    directive.templateUrl = "../Demo/directiveTemplate.html";
+
+    return directive;
+});
+
+myapp.directive('userinfo', function() {
+    var directive = {};
+    directive.restrict = 'E';
+    directive.templateUrl = "../Demo/userinfo-template.html";
+
+});
+
+myapp.directive('fundooRating', function() {
+
+    return {
+        restrict: 'A',
+        template: '<ul class="rating">' +
+                '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+                '\u2605' +
+                '</li>' +
+                '</ul>',
+        scope: {
+            ratingValue: '=',
+            max: '=',
+            onRatingSelected: '&'
+        },
+        link: function(scope, elem, attrs) {
+
+            var updateStars = function () {
+                scope.stars = [];
+                for (var i = 0; i < scope.max; i++) {
+                    scope.stars.push({ filled: i < scope.ratingValue });
+                }
+            };
+          
+            scope.toggle = function(index) {
+                scope.ratingValue = index + 1;
+                scope.onRatingSelected({ newRating: index + 1 });
+            };
+
+            scope.$watch('ratingValue', function (oldVal, newVal) {
+                if (newVal) {
+                    updateStars();
+                }
+            });
+
+        }
+    };
+});
